ae849f95f0f0148423e80c4ba735050981272a20
igordantasgf <igordantasgf@gmail.com>
2023-07-03 17:39:07 -0300

    Modificacao geral

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 class FileClient:
     def __init__(self, host, port):
@@ -6,27 +7,41 @@ class FileClient:
         self.port = port
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((self.host, self.port))
+        self.sockets = {}
+        self.stock = {}
 
-    def deposit(self, file_name, replication_level):
-        data = open(file_name,'r').read()
-        response_data = f"DEPOSIT:{file_name}:{data}"
+    def deposit(self, file_name, n_copies):
+        response_data = f"DEPOSIT:{file_name}:{n_copies}"
 
         self.client_socket.send(response_data.encode())
         print(self.client_socket.recv(1024).decode())
-        self.client_socket.close()
+        print("\nDepositado.")
 
+        time.sleep(0.1)  # Add a small delay (e.g., 100 milliseconds)
+        self.client_socket.shutdown(socket.SHUT_RDWR)
+        self.client_socket.close()
 
     def retrieve(self, file_name):
-        response_data = f"RETRIEVE:{file_name}"
+      response_data = f"RETRIEVE:{file_name}"
 
-        self.client_socket.send(response_data.encode())
-        print(self.client_socket.recv(1024).decode())
-        self.client_socket.close()
+      self.client_socket.sendall(response_data.encode())
+
+      with open(file_name, "wb") as file:
+        while True:
+          chunk = self.client_socket.recv(4096)
+          if not chunk:
+            break
+          file.write(chunk)
+
+      print('\nArquivo recebido com sucesso.')
+
+      time.sleep(0.1)  # Add a small delay (e.g., 100 milliseconds)
+      self.client_socket.shutdown(socket.SHUT_RDWR)
+      self.client_socket.close()
 
 
 if __name__ == "__main__":
-
     client = FileClient("localhost", 8000)
-    client.deposit("sample.txt",3)
-    # client.send_request("DEPOSIT:sample.txt:3")  # Exemplo de depósito de arquivo com 3 réplicas
-    # client.send_request("RETRIEVE:sample.txt")  # Exemplo de recuperação de arquivo
+    client.deposit("teste.txt", 2)
+    #client.retrieve("teste.txt")
+    client.close()
--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-
+import os
 
 class FileServer:
     SIZE = 1024
@@ -8,6 +8,8 @@ class FileServer:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.sockets = {}
+        self.files = {}
 
     def start(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,28 +22,69 @@ class FileServer:
             print(f"Conexão estabelecida com {address[0]}:{address[1]}")
             threading.Thread(target=self.handle_client, args=(client_socket,)).start()
 
+    def depositar(self, request_args):
+      # Implementar a lógica de replicação adequada para armazenar o arquivo em diferentes locais
+      _, file_name, n_copies = request_args
+
+      with open(file_name, 'rb') as file:
+          data = file.read()
+
+      # Checa quantidade de aplicações disponíveis
+      if int(n_copies) > len(self.sockets):
+        for i in range(len(self.sockets),int(n_copies)):
+          port = 8000 + i + 1
+          newSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          newSocket.bind(('localhost', port))
+          newSocket.listen(5)
+          self.sockets[port] = newSocket
+          print("New port: {}:{}".format('localhost', port))
+
+      for i in range(int(n_copies)):
+        socket_obj = self.sockets[8000+i+1]
+        self.files[8000+i+1] = file_name
+
+        #socket_obj.connect(('localhost',8000+i+1))
+        socket_obj.sendall(data)
+
+      return True
+
+    def retornar(self, request_args):
+      _, file_name = request_args
+      for porta in self.files:
+        if self.files[porta] == file_name:
+          socket_obj = self.sockets[porta]
+
+          with open(file_name, "wb") as file:
+            while True:
+              data = socket_obj.recv(4096)
+              if not data:
+                break
+              file.write(data)
+      return True   
+
     def handle_client(self, client_socket):
         request = client_socket.recv(self.SIZE).decode()
         request_args = request.split(":")
         command = request_args[0]
 
         if command == "DEPOSIT":
-            # Implementar a lógica de replicação adequada para armazenar o arquivo em diferentes locais
-            _, file_name, data = request_args
-            open(file_name, "w").write(data)
-            response = "Depósito concluído com sucesso."
+          if self.depositar(request_args):
+            response = "Deposito realizado com sucesso"
+          else:
+             response = "Nao foi possivel realizar o deposito"
 
         elif command == "RETRIEVE":
-            # Implementar a lógica de recuperação adequada para encontrar o arquivo replicado e devolver ao cliente
-            _, file_name = request_args
-            response = open(file_name, "r").read()
-
+          if self.retornar(request_args):
+            response = "Arquivo retornado com sucesso"
+          else:
+             response = "Nao foi possivel encontrar arquivo solicitado"
+        
         else:
             response = "Comando inválido."
 
         client_socket.send(response.encode())
         client_socket.close()
-
+        
 
 if __name__ == "__main__":
     # Execução do servidor
