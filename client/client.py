ca11fcbab01608db18a0d748bea9eb5c58d17f47
igordantasgf <igordantasgf@gmail.com>
2023-07-05 22:32:29 -0300

    modf

--- client/client.py
+++ client/client.py
@@ -12,35 +12,18 @@ class FileClient:
     def deposit(self, file_name, n_copies):
       if n_copies > len(self.sockets):
         for i in range(len(self.sockets),n_copies):
-           self.sockets.append(8001+i)
+          port = 8001+i
+          self.sockets.append(port)
 
-      data = open(file_name,'r').read()
-      response_data = f"DEPOSIT:{file_name}:{data}:{n_copies}"
+          data = open(file_name,'r').read()
+          response_data = f"DEPOSIT:{file_name}:{data}:{port}"
 
-      self.client_socket.send(response_data.encode())
-      self.client_socket.shutdown(socket.SHUT_RDWR)
-      self.client_socket.accept()
-      print(self.client_socket.recv(1024).decode())
-      self.client_socket.close()
-
-    def retrieve(self, file_name):
-      response_data = f"RETRIEVE:{file_name}"
-
-      self.client_socket.sendall(response_data.encode())
-
-      with open(file_name, "wb") as file:
-        while True:
-          chunk = self.client_socket.recv(4096)
-          if not chunk:
-            break
-          file.write(chunk)
-
-      print('\nArquivo recebido com sucesso.')
-
-      time.sleep(0.1)  # Add a small delay (e.g., 100 milliseconds)
-      self.client_socket.shutdown(socket.SHUT_RDWR)
-      self.client_socket.close()
-
+          self.client_socket.send(response_data.encode())
+          self.client_socket.close()
+          self.client_socket.connect((self.host, self.port))
+          print(self.client_socket.recv(1024).decode())
+          self.client_socket.close()
+      
 
 if __name__ == "__main__":
     client = FileClient("localhost", 8000)
--- server/server.py
+++ server/server.py
@@ -21,65 +21,50 @@ class FileServer:
             print(f"Conexão estabelecida com {address[0]}:{address[1]}")
             threading.Thread(target=self.handle_client, args=(client_socket,)).start()
 
-    def criar_sockets(self, n_copies):
-       for i in range(len(self.sockets),int(n_copies)):
-          port = 8001 + i
-          newSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-          newSocket.bind(('localhost', port))
-          newSocket.listen(5)
-          self.sockets[port] = newSocket
-          print("New port: {}:{}".format('localhost', port))
+    def criar_sockets(self, porta):
+      newSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      newSocket.bind(('localhost', int(porta)))
+      newSocket.listen(5)
+      self.sockets[porta] = newSocket
+      print(f"New port: {'localhost'}:{porta}")
 
-    # Socket replica aceita requisições do server_socket
-    def abrir_deposito(self, socket, porta):
-      origin, address = socket.accept()
-      request = origin.recv(self.SIZE).decode()
-      request_args = request.split(":")
+      t = threading.Thread(target=self.run_sockets, args=(newSocket,porta))
+      t.daemon = True
+      t.start()
 
-      print(f"Conexão estabelecida com {address[0]}:{address[1]}")
-      _, file_name, data = request_args
-      with open(f"replicas/{porta}/{file_name}", "w") as file:
-         file.write(data)
+    # Thread de cada socket (replica) rodando
+    def run_sockets_deposit(self, socket, port):
+      while True:
+        print(f"Esperando conexão na porta {port}")
+        origin, address = socket.accept()
+        print(f"Conexão estabelecida com {address[0]}:{address[1]} na porta {port}")
 
-      socket.send(f"Armazenado na porta {porta}".encode())
-      socket.shutdown(socket.SHUT.RDWR)
-      socket.close()
+        request = origin.recv(self.SIZE).decode()
+        request_args = request.split(":")
+        _, file_name, data = request_args
+
+        with open(f"replicas/{port}/{file_name}", "w") as file:
+          file.write(data)
+      
+        origin.send("Arquivo armazenado com sucesso".encode())
+        origin.close()
 
     def depositar(self, request):
       # Implementar a lógica de replicação adequada para armazenar o arquivo em diferentes locais 
       request_args = request.split(":")
-      _, file_name, data, n_copies = request_args
+      _, file_name, data, porta = request_args
 
       # Checa quantidade de aplicações disponíveis
-      if int(n_copies) > len(self.sockets):
-        self.criar_sockets(n_copies)
-      
-      for i in range(8001, 8001+int(n_copies)):
-        threading.Thread(target=self.abrir_deposito, name=f"Thread:{i}", args=(self.sockets[i], i,)).start()
-        self.server_socket.connect((self.host,i))
-        self.server_socket.sendall(f"DADOS:{file_name}:{data}:{n_copies}".encode())
-        self.server_socket.shutdown(socket.SHUT_RDWR)
-        time.sleep(0.1)
+      if porta not in self.sockets:
+        self.criar_sockets(porta)
+
       return True
-
-    def retornar(self, request_args):
-      _, file_name = request_args
-      for porta in self.files:
-        if self.files[porta] == file_name:
-          socket_obj = self.sockets[porta]
-
-          with open(file_name, "wb") as file:
-            while True:
-              data = socket_obj.recv(4096)
-              if not data:
-                break
-              file.write(data)
-      return True   
 
     def handle_client(self, client_socket):
         request = client_socket.recv(self.SIZE).decode()
         request_args = request.split(":")
         command = request_args[0]
+        time.sleep(0.2)
 
         if command == "DEPOSIT":
           if self.depositar(request):
@@ -87,11 +72,11 @@ class FileServer:
           else:
              response = "Nao foi possivel realizar o deposito"
 
-        elif command == "RETRIEVE":
-          if self.retornar(request_args):
-            response = "Arquivo retornado com sucesso"
-          else:
-             response = "Nao foi possivel encontrar arquivo solicitado"
+        #elif command == "RETRIEVE":
+        #  if self.retornar(request_args):
+        #    response = "Arquivo retornado com sucesso"
+        #  else:
+        #     response = "Nao foi possivel encontrar arquivo solicitado"
         
         else:
             response = "Comando inválido."
